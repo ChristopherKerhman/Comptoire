1cb784a7880be7a373b76a582dd5986659c4b610
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2021-06-02 14:02:38 +0200

    Problème d'image résolus par Cyril

--- src/components/coupes.vue
+++ src/components/coupes.vue
@@ -1,12 +1,6 @@
 <template>
 <h3 class="titre">Nos Coupes glacées</h3>
 <div class="conteneurMenu">
-  <figure>
-    <img src="@/images/bounty.jpg" alt="Coupe Glace mexicaine" />
-    <figcaption>Coupe Glacer : 8.90 €
-      <button class="bouttonAdd" v-on:click="Ajouter(8.90, Coupe)">Ajouter</button>
-    </figcaption>
-    </figure>
     <figure v-for="boule in coupes" v-bind:key="boule">
       <img :alt="boule.nom" v-bind:src="boule.cornet" />
       <p>Composition : {{boule.composition}}</p>
@@ -30,10 +24,10 @@ export default {
       panier: 0,
       liste: [],
       coupes: [
-        { id: 0, cornet: './images/glace-mexicaine.jpg', prix: 7.9, nom: 'Coupe Mexicaine', composition: 'Chantilly, boule vanille bourbon, glace café, coulis de café chaud' },
-        { id: 1, cornet: './images/amarena.jpg', prix: 7.9, nom: 'Amarena', composition: 'Chantilly, boule vanille bourbon, glace Cerise, cerise confite' },
-        { id: 2, cornet: './images/bounty.jpg', prix: 7.9, nom: 'bounty', composition: 'Chantilly, boule vanille bourbon, glace Coco, boule chocolat' },
-        { id: 3, cornet: './images/coupe-exotique.jpg', prix: 8.9, nom: 'Coupe exotique', composition: 'Chantilly, boule mangue, boule banane, boule passion' }
+        { id: 0, cornet: require('@/images/glace-mexicaine.jpg'), prix: 7.9, nom: 'Coupe Mexicaine', composition: 'Chantilly, boule vanille bourbon, glace café, coulis de café chaud' },
+        { id: 1, cornet: require('@/images/amarena.jpg'), prix: 7.9, nom: 'Amarena', composition: 'Chantilly, boule vanille bourbon, glace Cerise, cerise confite' },
+        { id: 2, cornet: require('@/images/bounty.jpg'), prix: 7.9, nom: 'bounty', composition: 'Chantilly, boule vanille bourbon, glace Coco, boule chocolat' },
+        { id: 3, cornet: require('@/images/coupe-exotique.jpg'), prix: 8.9, nom: 'Coupe exotique', composition: 'Chantilly, boule mangue, boule banane, boule passion' }
       ]
     }
   },
